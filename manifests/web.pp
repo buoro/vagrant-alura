6abf4d45587049a00615d816614258d44499a511
Victor Buoro da Silva <victor.buoro.silva@usp.br>
2017-08-26 16:43:40 -0300

    maquina criada, configuração para download dos pacotes tomcat, openjdk, mysql e download do projeto vraptor-musicjungle

---
+++ manifests/web.pp
@@ -0,0 +1,30 @@
+exec { "apt-update":
+  command => "/usr/bin/apt-get update"
+}
+
+package { ["openjdk-7-jre", "tomcat7", "mysql-server"]:
+  ensure => installed,
+  require => Exec["apt-update"]
+}
+
+exec { "download-vraptor-musicjungle":
+  command => "/usr/bin/wget https://github.com/alura-cursos/provisionamento-com-vagrant-e-puppet/raw/master/manifests/vraptor-musicjungle.war -P /vagrant/manifests",
+  unless => "/bin/cat /vagrant/manifests/vraptor-musicjungle.war"
+}
+
+service { "tomcat7":
+  ensure => running,
+  enable => true,
+  hasstatus => true,
+  hasrestart => true,
+  require => Package["tomcat7"]
+}
+
+file { "/var/lib/tomcat7/webapps/vraptor-musicjungle.war":
+  source => "/vagrant/manifests/vraptor-musicjungle.war",
+  owner => "tomcat7",
+  group => "tomcat7",
+  mode => 0644,
+  require => Package["tomcat7"],
+  notify => Service["tomcat7"]
+}
